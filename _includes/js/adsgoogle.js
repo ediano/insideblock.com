77e6197e351805dca15b9ed7e394ee3716b1f846
Ediano Silva Santos <ess.ediano@gmail.com>
2019-11-19 10:11:54 -0400

    Update

--- _includes/js/adsgoogle.js
+++ _includes/js/adsgoogle.js
@@ -2,5 +2,5 @@ const scriptAds = document.createElement('script');
 scriptAds.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
 scriptAds.setAttribute('async','');
 setTimeout(function() {
-    document.head.appendChild(scriptAds);
+    document.body.appendChild(scriptAds);
 }, 3000);
