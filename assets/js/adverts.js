ec317aabc614459a11eeaabc166ac52338edd508
Ediano Silva Santos <ess.ediano@gmail.com>
2019-11-19 19:28:50 -0400

    Update adverts.js

--- assets/js/adverts.js
+++ assets/js/adverts.js
@@ -4,19 +4,14 @@ if (adverts == true) {
     scriptAdsense.setAttribute('async', '');
     scriptAdsense.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
     
-    const insAdsense = document.createElement('ins');
-    insAdsense.setAttribute('class', 'adsbygoogle');
-    insAdsense.setAttribute('style', 'display:block; text-align:center');
-    insAdsense.setAttribute('data-ad-layout', 'in-article');
-    insAdsense.setAttribute('data-ad-format', 'fluid');
-    insAdsense.setAttribute('data-ad-client', 'ca-pub-6753037893956918');
-    insAdsense.setAttribute('data-ad-slot', '6049951967');
-    
     setTimeout(function() {
         document.head.appendChild(scriptAdsense);
         let ads = document.querySelectorAll('.ads');
         for(let i = 0; i < ads.length; i++){
-            ads[i].appendChild(insAdsense);
+            ads[i].innerHTML = `
+                <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-6753037893956918" data-ad-slot="6049951967"></ins>
+                <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+            `;
         }
     }, 3000);
 }
