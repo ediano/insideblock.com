bda77e827a5a68f9d2e524de74fa1955e1758ef1
Ediano Silva Santos <ess.ediano@gmail.com>
2019-11-19 19:36:10 -0400

    Update adverts.js

--- assets/js/adverts.js
+++ assets/js/adverts.js
@@ -1,16 +1,19 @@
 adverts = true;
-if (adverts == true) {
-    const scriptAdsense = document.createElement('script');
-    scriptAdsense.setAttribute('async', '');
-    scriptAdsense.setAttribute('src', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
-    
+if (adverts == true) {    
     setTimeout(function() {
         document.head.appendChild(scriptAdsense);
-        let ads = document.querySelectorAll('.ads');
         for(let i = 0; i < ads.length; i++){
             ads[i].innerHTML = `
-                <ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-6753037893956918" data-ad-slot="6049951967"></ins>
-                <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <ins class="adsbygoogle"
+                    style="display:block; text-align:center;"
+                    data-ad-layout="in-article"
+                    data-ad-format="fluid"
+                    data-ad-client="ca-pub-6753037893956918"
+                    data-ad-slot="6049951967"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
             `;
         }
     }, 3000);
