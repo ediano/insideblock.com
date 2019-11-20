8b3d15c08eab27d3cc92edbfc6d94db34a02026a
Ediano Silva Santos <ess.ediano@gmail.com>
2019-11-19 20:18:42 -0400

    Update adverts.js

--- assets/js/adverts.js
+++ assets/js/adverts.js
@@ -7,16 +7,11 @@ if (adverts == true) {
     setTimeout(function() {
         for(let i = 0; i < ads.length; i++){
             ads[i].innerHTML = `
-                <ins class="adsbygoogle"
-                    style="display:block; text-align:center;"
-                    data-ad-layout="in-article"
-                    data-ad-format="fluid"
-                    data-ad-client="ca-pub-6753037893956918"
-                    data-ad-slot="6049951967">
+                <ins class="adsbygoogle" style="display:block; text-align:center;"
+                    data-ad-layout="in-article" data-ad-format="fluid"
+                    data-ad-client="ca-pub-6753037893956918" data-ad-slot="6049951967">
                 </ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
             `;
         }
         document.head.appendChild(scriptAdsense);
